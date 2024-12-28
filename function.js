1395b5003ab30de6a6fdcc7f31d86248ada541bc
Ademola309 <oyajimiademola@gmail.com>
2024-12-28 04:04:49 -0800

    third push

---
+++ function.js
@@ -0,0 +1,5 @@
+function areaofaTriangle (length, width){
+    return length * width
+}
+let result = areaofaTriangle(4,9)
+console.log(result)
---
+++ object.js
@@ -0,0 +1,37 @@
+// My Personal Library
+
+const book = {
+     title : "The country Nigeria",
+    description : "comedy",
+    numberofPages : 2024 ,
+    author : "Jimmy Ademola" ,
+    reading : false
+} 
+console.log(book)
+
+if (book.reading === true){
+    console.log(true)
+}
+else {
+    console.log(false)
+}
+
+// My Personal Library 2
+// const book = {
+//     title: "The country Nigeria",
+//     description: "comedy",
+//     numberOfPages: 2024,
+//     author: "Jimmy Ademola",
+//     reading: false
+//   }
+  
+//   function toggleReadingStatus() {
+//     book.reading = !book.reading;
+//   }
+  
+//   console.log(book.reading); // Output: false
+//   toggleReadingStatus();
+//   console.log(book.reading); // Output: true
+//   toggleReadingStatus();
+//   console.log(book.reading); // Output: false
+  
